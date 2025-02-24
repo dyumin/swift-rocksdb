99df0a431077014e9cf383727aa50755d5bf8817
dyumin <jejni.j@gmail.com>
2025-02-24 23:36:29 +0300

    // empty cpp cannot be removed because https://forums.swift.org/t/header-only-library-using-swift-package-manager/42700

--- Sources/cpp-intepop/rocksdb_cpp_interop.cpp
+++ Sources/cpp-intepop/rocksdb_cpp_interop.cpp
@@ -1,3 +1,4 @@
 #include "rocksdb_cpp_interop.h"
 
+// empty cpp cannot be removed because https://forums.swift.org/t/header-only-library-using-swift-package-manager/42700
 namespace swiftrocks {} // namespace swiftrocks
