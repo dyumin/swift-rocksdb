ac520a9422df0c330bdd97bf7056fe2afae15a71
Alexey Dyumin <7953339+dyumin@users.noreply.github.com>
2025-08-13 01:45:55 +0300

    downgrade swift-tools-version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -342,5 +342,5 @@ let package = Package(
             ]),
     ],
 
-    cxxLanguageStandard: .cxx20
+    cxxLanguageStandard: .cxx17
 )
